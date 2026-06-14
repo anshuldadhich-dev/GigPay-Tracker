e9f6fee666a86f6721e07c9d982d74a2e6631bc5
anshuldadhich-dev <a.danshul2005@gmail.com>
2026-06-14 22:12:24 +0530

    Pdf fixes

--- services/pdfService.js
+++ services/pdfService.js
@@ -735,8 +735,17 @@ function buildPDFTemplate(data = {}) {
 
 async function generatePDF(data = {}) {
   const browser = await puppeteer.launch({
-    headless: 'new',
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+    ],
   });
   try {
     const page = await browser.newPage();
